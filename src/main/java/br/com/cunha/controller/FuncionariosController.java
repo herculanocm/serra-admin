1e405b73b8ff86e537922f1a3c25a6f9fa6441e2
Herculano da Cunha Madureira <herculanocm@acs.com.br>
2017-03-14 22:28:08 -0300

    configurado o log4j

--- src/main/java/br/com/cunha/controller/FuncionariosController.java
+++ src/main/java/br/com/cunha/controller/FuncionariosController.java
@@ -2,14 +2,25 @@ package br.com.cunha.controller;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.view.Results;
 import br.com.cunha.interfaces.RepoFuncionarioI;
+import br.com.cunha.modelo.Funcionario;
+
+
 
 @Controller
 public class FuncionariosController {
 
+	private static final Logger logger = LoggerFactory.getLogger(FuncionariosController.class);
+	
 	private RepoFuncionarioI repoFuncionarioI;
 	private Result result;
 
@@ -26,9 +37,21 @@ public class FuncionariosController {
 		this.result = result;
 	}
 	
-	@Get("/funcionarios")
-	public void getFuncionarios(){
-		result.include("realizado com sucesso");
+	
+	@Get @Path("/funcionarios")
+	public void lista(){
+		logger.info("Testando o comportamento do LOG.");
+		Funcionario f = new Funcionario();
+		f.setId(new Long(1));
+		f.setNome("herculano");
+		f.setMatricula("448383");
+		
+		result.use(Results.json()).from(f).serialize();
+	}
+	
+	@Post @Path("/funcionario")
+	public void salva(Funcionario funcionario){
+		
 	}
 	
 	
